f38054bf6ae359e1cd3bbf3063c7290e2b19f988
kirangadalay <gadalaykiran@gmail.com>
2019-02-24 22:48:39 -0500

    commit

---
+++ firstproject/src/firstproject/addition.java
@@ -0,0 +1,14 @@
+package firstproject;
+
+public class addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ int a , b, c;
+  a = 25;
+  b = 25;
+ c = a + b;
+System.out.println(c);
+	}
+
+}
