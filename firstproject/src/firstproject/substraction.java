037c86b4c91283e533d67b2c08f1063b36058788
kirangadalay <gadalaykiran@gmail.com>
2019-02-24 23:23:38 -0500

    substraction

---
+++ firstproject/src/firstproject/substraction.java
@@ -0,0 +1,13 @@
+package firstproject;
+
+public class substraction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	 int x, y, z ;
+	  x = 100;
+	  y = 30;
+	 z = x - y;
+	System.out.println(z);
+}
+}
